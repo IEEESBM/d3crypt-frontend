eebadbf48b092ce1d961e07595f1371ddd3b6599
yashgul <76999990+yashgul@users.noreply.github.com>
2021-12-24 14:21:01 +0530

    Update FAQ.jsx

--- src/Components/FAQs/FAQ.jsx
+++ src/Components/FAQs/FAQ.jsx
@@ -1,46 +1,30 @@
 import React from "react";
 import "./FAQ.css";
-import Accordion from "./Accordian";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Popper from 'popper.js';
+import {Accordion} from 'react-bootstrap';
 export default function FAQs() {
-  const rules = [
-    {
-      title: " Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus metus, porta ac ipsum ornare, ornare consequat augue. Integer ornare mollis bibendum. Nunc in rutrum sem, a bibendum ex. Donec nec placerat ante, vitae ornare.",
-    },
-    {
-      title: " Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus metus, porta ac ipsum ornare, ornare consequat augue. Integer ornare mollis bibendum. Nunc in rutrum sem, a bibendum ex. Donec nec placerat ante, vitae ornare.",
-    },
-    {
-      title: " Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus metus, porta ac ipsum ornare, ornare consequat augue. Integer ornare mollis bibendum. Nunc in rutrum sem, a bibendum ex. Donec nec placerat ante, vitae ornare.",
-    },
-    {
-      title: " Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus metus, porta ac ipsum ornare, ornare consequat augue. Integer ornare mollis bibendum. Nunc in rutrum sem, a bibendum ex. Donec nec placerat ante, vitae ornare.",
-    },
-    {
-      title: " Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacus metus, porta ac ipsum ornare, ornare consequat augue. Integer ornare mollis bibendum. Nunc in rutrum sem, a bibendum ex. Donec nec placerat ante, vitae ornare.",
-    },
-  ];
+  
   return (
     <div className="rules-container">
       <div className="rules-h">FAQs</div>
       <div class="accordion" id="accordionExample">
+
+
   <div class="accordion-item">
-    <h2 class="accordion-header" id="headingOne">
+    <div class="accordion-header" id="headingOne">
       <button class="accordion-button" type="button" data-bs-toggle="collapse" id="acc" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-        Accordion Item #1
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit
       </button>
-    </h2>
+    </div>
     <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </div>
     </div>
   </div>
+
+
   <div class="accordion-item">
     <h2 class="accordion-header" id="headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" id="acc" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
@@ -53,25 +37,23 @@ export default function FAQs() {
       </div>
     </div>
   </div>
+
+
   <div class="accordion-item">
-    <h2 class="accordion-header" id="headingThree">
+    <div class="accordion-header" id="headingThree">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" id="acc" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
         Accordion Item #3
       </button>
-    </h2>
+    </div>
     <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
       <div class="accordion-body">
-        <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
       </div>
     </div>
   </div>
 </div>
+</div>
 
-      {rules.map((rule) => (
-        <div>
-          {/* <Accordion title={rule.title}>{rule.desc}</Accordion> */}
-        </div>
-      ))}
-    </div>
+     
   );
 }
