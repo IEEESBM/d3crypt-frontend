419193d13b6fdd5b19a0919167a112784718a061
DataGradOrg <suryaansh2002@gmail.com>
2022-01-20 19:56:40 +0530

    Home page ui

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -4,15 +4,14 @@ import insta from "../../assets/instagram.svg";
 import mention from "../../assets/mention.svg";
 import net from "../../assets/net.svg";
 import logo from "../../assets/ieee_logo.svg";
+import youtube from "../../assets/youtube.svg";
+
 import ig_logo from "../../assets/Instagram_Icon.svg";
 import twitter_logo from "../../assets/Twitter_Icon.svg";
 import fb_logo from "../../assets/Facebook_Icon.svg";
 
-import { SiFacebook,SiInstagram } from 'react-icons/si';
-import { AiFillTwitterCircle } from 'react-icons/ai';
-
-
-
+import { SiFacebook, SiInstagram } from "react-icons/si";
+import { AiFillTwitterCircle } from "react-icons/ai";
 
 export default function Contact() {
   return (
@@ -22,17 +21,41 @@ export default function Contact() {
       </div>
       <div className="contact-row">
         <div className="c-box">
-          <img src={net} className="contact-img" />
-          <div className="c-box-text">www.ieeesbm.com</div>
+          <a
+            href="
+        https://ieeemanipal.com/"
+            target={"_blank"}
+          >
+            <img src={net} className="contact-img" />
+            <div className="c-box-text">www.ieeesbm.com</div>
+          </a>
         </div>
 
         <div className="c-box">
-          <img src={mention} className="contact-img" />
-          <div className="c-box-text">info@ieeesbm.com</div>
+          <a
+            href="
+       mailto:info@ieeesbm.com 
+        "
+            target={"_blank"}
+          >
+            <img src={mention} className="contact-img" />
+            <div className="c-box-text">info@ieeesbm.com</div>
+          </a>
         </div>
         <div className="c-box">
-          <img src={insta} className="contact-img" />
-          <div className="c-box-text">ieeesbm</div>
+          <a href="https://www.instagram.com/ieeesbm/?hl=en" target={"_blank"}>
+            <img src={insta} className="contact-img" />
+            <div className="c-box-text">ieeesbm</div>
+          </a>
+        </div>
+        <div className="c-box">
+          <a
+            href="https://www.youtube.com/channel/UCeK19_N4m-37SiiUxSx1jzg"
+            target={"_blank"}
+          >
+            <img src={youtube} className="contact-img" />
+            <div className="c-box-text">IEEE SBM</div>
+          </a>
         </div>
       </div>
 
@@ -43,12 +66,12 @@ export default function Contact() {
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </div>
         <div>
-        <SiFacebook className="footer-icon"/>
-        <AiFillTwitterCircle className="footer-icon"/>
+          <SiFacebook className="footer-icon" />
+          <AiFillTwitterCircle className="footer-icon" />
           {/* <img src={fb_logo} className="footer-icon" /> */}
           {/* <img src={twitter_logo} className="footer-icon" /> */}
           {/* <img src={ig_logo} className="footer-icon" /> */}
-          <SiInstagram className="footer-icon"/>
+          <SiInstagram className="footer-icon" />
         </div>
       </div>
     </div>
